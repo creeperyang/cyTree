62bda1f063de9e717fd7890ea1d16e0e55f03a04
creeperyang <yangcreeper@hotmail.com>
2015-03-23 19:55:42 +0800

    finish cyTree @version 0.0.1

--- src/cy-tree.js
+++ src/cy-tree.js
@@ -5,11 +5,135 @@
 
 var app = angular.module('cyTree', []);
 
+/**
+ * @ngdoc service
+ * @name cyTree.factory:cyTreeUnits
+ * @description utils for cyTree
+ */
 app.service('cyTreeUtils', [function() {
 
     var uniqueTreeId = 0,
         prefix = 'cyTree',
         tplCache = {};
+
+    // rules: for parent nodes
+    /* 1. child not change, stop handling
+     * 2. child selected --> nonSelected, parent selectedCount--, normal handling
+     * 3. child selected --> partSelected, parent selectedCount--, (if count=0)special handling
+     * 4. child partSelected --> nonSelected, parent (if selected, do nothing and stop handling)/(if partSelected, nonSelected)
+     * 5. child partSelected --> selected, parent selectedCount++, normal handling
+     * 6. child nonSelected --> selected, parent selectedCount++, normal handling
+     * 7. child nonSelected --> partSelected, special handling
+    */
+    var nodeHandlingRules = {
+        // parentNode must be partSelected/selected
+        's2n': function(parentNode, children) {
+            var status, 
+                len, partSelectedCount = 0;
+            parentNode.selectedCount--;
+            if(parentNode.selected) {
+                if(parentNode.selectedCount === 0) {
+                    status = 's2n';
+                    parentNode.nonSelected = true;
+                } else {
+                    status = 's2p';
+                    parentNode.partSelected = true;
+                }
+                parentNode.selected = false;
+            } else { // partSelected
+                if(parentNode.selectedCount > 0) {
+                    status = 'stable';
+                } else {
+                    len = children.length;
+                    while(len--) {
+                        if(children[len].partSelected) {
+                            partSelectedCount++;
+                        }
+                    }
+                    status = partSelectedCount > 0 ? 'stable' : 'p2n';
+                }
+                if(status === 'p2n') {
+                    parentNode.partSelected = false;
+                    parentNode.nonSelected = true;
+                }
+            }
+            return status;
+        },
+        // parentNode must be partSelected/selected
+        's2p': function(parentNode) { // selectedCount>=1
+            var status;
+            parentNode.selectedCount--;
+            if(parentNode.selected) {
+                status = 's2p';
+            } else { // it is surly partSelected
+                status = 'stable';
+            }
+            parentNode.partSelected = true;
+            parentNode.selected = false;
+            return status;
+        },
+        // parentNode must be partSelected
+        'p2n': function(parentNode) {
+            var status;
+            if(parentNode.selectedCount === 0) {
+                status = 'p2n';
+                parentNode.partSelected = false;
+                parentNode.nonSelected = true;
+            } else { // it is still partSelected
+                status = 'stable';
+            }
+            return status;
+        },
+        // parentNode must be partSelected
+        'p2s': function(parentNode, children) {
+            var status;
+            parentNode.selectedCount++;
+            if(parentNode.selectedCount === children.length) {
+                status = 'p2s';
+                parentNode.partSelected = false;
+                parentNode.selected = true;
+            } else { // it is still partSelected
+                status = 'stable';
+            }
+            return status;
+        },
+        // parentNode can be partSelected/nonSelected
+        'n2s': function(parentNode, children) {
+            var status;
+            parentNode.selectedCount++;
+            if(parentNode.partSelected) { // still partSelected
+                if(parentNode.selectedCount === children.length) {
+                    status = 'p2s';
+                    parentNode.selected = true;
+                    parentNode.partSelected = false;
+                } else {
+                    status = 'stable';
+                }
+            } else { // parentNode is nonSelected
+                parentNode.nonSelected = false;
+                if(children.length > 1) {
+                    parentNode.partSelected = true;
+                    status = 'n2p';
+                } else {
+                    parentNode.selected = true;
+                    status = 'n2s';
+                }
+            }
+            return status;
+        },
+        // parentNode can be partSelected/nonSelected
+        'n2p': function(parentNode, children) {
+            var status;
+            if(parentNode.partSelected) { // still partSelected
+                status = 'stable';
+            } else {
+                parentNode.partSelected = true;
+                parentNode.nonSelected = false;
+                status = 'n2p';
+            }
+            return status;
+        }
+    };
     
     var genOriginalTemplate = function(treeData, childFlag, labelFlag) {
         return '<ul>' +
@@ -62,9 +186,9 @@ app.service('cyTreeUtils', [function() {
 
     /**
      * @ngdoc method
-     * @name cyTree.service:cyTreeUtils#genUniqueId
+     * @name cyTree.service:cyTreeUtils#getTreeCount
      * @methodOf cyTree.service:cyTreeUtils
-     * @description generate unique tree id
+     * @description trees count
      *
      * @returns {number} trees count
      */
@@ -108,16 +232,39 @@ app.service('cyTreeUtils', [function() {
      * @ngdoc method
      * @name cyTree.service:cyTreeUtils#iterateDown
      * @methodOf cyTree.service:cyTreeUtils
-     * @description iterate tree data
+     * @description iterate down tree data
      *
      * @param {array} array tree data
      * @param {string} childFlag child list property name
-     * @param {object} parent parent node
-     * @param {function} callback when iterate treedata, invoke for every node data
+     * @param {object} parent parent node(if not exist, set null)
+     * @param {function} callback when iterate treedata, invoke for every node
+     * @param {function} stop whether stop iterating down for current node
      */
     this.iterateDown = downTree;
+    
+    /**
+     * @ngdoc method
+     * @name cyTree.service:cyTreeUtils#iterateUp
+     * @methodOf cyTree.service:cyTreeUtils
+     * @description iterate up tree data
+     *
+     * @param {object} node node of tree data
+     * @param {string} parentFlag parent property name, typically 'parent'
+     * @param {function} callback when iterate treedata, invoke for every node
+     * @param {function} stop whether stop iterating up
+     */
     this.iterateUp = upTree;
 
+    /**
+     * @ngdoc method
+     * @name cyTree.service:cyTreeUtils#templateCache
+     * @methodOf cyTree.service:cyTreeUtils
+     * @description get or set sub tree template
+     *
+     * @param {string} treeId tree id
+     * @param {string} tpl optional template string, if exists, set template, otherwise, retrive template
+     * @return {string} if tpl not exists, return the template correspond with treeId
+     */
     this.templateCache = function(treeId, tpl) {
         if(typeof tpl === 'string') {
             tplCache[treeId] = tpl;
@@ -126,8 +273,29 @@ app.service('cyTreeUtils', [function() {
         }
     }
 
+    // handle node click, update parent nodes' status
+    // Although the method is exposed, don't change it or invoke it directly unless you're experienced.
+    this.handlingNodeUp = function(node, rule, parentFlag, childFlag) {
+        upTree(node, parentFlag, function(parentNode, curNode) {
+            if(!parentNode) {
+                return;
+            }
+            rule = nodeHandlingRules[rule](parentNode, parentNode[childFlag]);
+        }, function(parentNode, curNode) { // stop iteration condition
+            return !parentNode || rule === 'stable';
+        });
+    }
+
 }]);
 
+/**
+ * @ngdoc directive
+ * @name cyTree.directive:cyTree
+ * @restrict A
+ *
+ * @description
+ * Directive that handle data, add behaviour and gen template
+ */
 app.directive('cyTree', ['$log', '$compile', 'cyTreeUtils',
     function($log, $compile, cyTreeUtils) {
         return {
@@ -147,9 +315,10 @@ app.directive('cyTree', ['$log', '$compile', 'cyTreeUtils',
 
                 var treeId = cyTreeUtils.genUniqueId();
                 var subTemplate = cyTreeUtils.genTemplate('node.' + treeChildFlag, treeChildFlag, treeLabelFlag);
-
                 cyTreeUtils.templateCache(treeId, subTemplate);
-                log('set tpl', treeId)
+
+                log('cyTree:generate template and cache it', treeId);
+
                 return {
                     pre: function(scope, element, attrs, ctrl) {
                         var treeMethods = scope.treeMethods = {};
@@ -157,12 +326,12 @@ app.directive('cyTree', ['$log', '$compile', 'cyTreeUtils',
                         var keepRawData = false; // currently set to false
                         var watch;
 
-                        scope.treeInfo.treeId = treeId;
+                        treeInfo.treeId = treeId;
 
                         if(!keepRawData) {
                             watch = scope.$watch(treeData, function(newData, oldData) {
                                 if(newData !== undefined) {
-                                    console.log('cyTree:handle raw data', newData);
+                                    log('cyTree:about to handle raw data', newData);
                                     watch(); // remove watch;
                                     cyTreeUtils.iterateDown(scope[treeData], treeChildFlag, null, function(item, parent, children) {
                                         var count, 
@@ -188,7 +357,7 @@ app.directive('cyTree', ['$log', '$compile', 'cyTreeUtils',
                             });
                         }
 
-                        //Collapse or Expand node when collapse icon clicked
+                        //collapse or Expand node when collapse icon clicked
                         treeMethods.collapseNode = function(curNode) {
                             log('cyTree:collapseNode', treeId);
                             curNode.collapsed = !curNode.collapsed;
@@ -200,43 +369,41 @@ app.directive('cyTree', ['$log', '$compile', 'cyTreeUtils',
                             if(!node) {
                                 return;
                             }
-                            log('cyTree:selectNode', treeId);
 
-                            var original = node.nonSelected,
-                                toSelect = false;
-                            // 全选-->不选 部分-->不选 不选-->全选
-                            if(original) {
-                                node.selected = true;
-                                node.nonSelected = node.partSelected = false;
+                            log('cyTree:about to select node', node);
+
+                            var nonSelected = node.nonSelected,
+                                toSelect,
+                                rule;
+
+                            // rules: for node clicked
+                            // 1. selected, partSelected = true; --> nonSelected = true;
+                            // 2. nonSelected = true; --> selected = true;
+                            if(nonSelected) {
                                 toSelect = true;
+                                node.selected = true;
+                                node.nonSelected = false;
+                                rule = 'n2s';
+                                node.selectedCount = node[treeChildFlag] ? node[treeChildFlag].length : 0;
                             } else {
+                                toSelect = false;
+                                rule = node.selected ? 's2n' : 'p2n';
                                 node.nonSelected = true;
-                                node.selected = node.partSelected = false;
+                                node.selected = false;
+                                node.selectedCount = 0;
                             }
+                            node.partSelected = false;
+
                             treeInfo.currentSelectedNode = node;
                             
-                            // handle parent chain
-                            cyTreeUtils.iterateUp(node, 'parent', function(parentNode, curNode) {
-                                if(!parentNode) {
-                                    return;
-                                }
-                                if(toSelect) {
-                                    parentNode.selectedCount++;
-                                } else {
-                                    parentNode.selectedCount--;
-                                }
-                                setNodeStatus(parentNode, parentNode.selectedCount, parentNode[treeChildFlag].length);
-                            }, function(parentNode, curNode) { // whether to stop iterate
-                                if(!parentNode) {
-                                    return true;
-                                }
-                                return parentNode.partSelected && parentNode.selectedCount > (toSelect ? 1 : 0);
-                            });
+                            cyTreeUtils.handlingNodeUp(node, rule, 'parent', treeChildFlag);
 
                             if(node.leafNode) {
                                 return;
                             }
-                            // handle children
+
+                            // rules: for child nodes
+                            // 1. just the same status with current node
                             cyTreeUtils.iterateDown([node], treeChildFlag, null, 
                                 function(item, parent, children) {
                                     var len = 0;
@@ -280,11 +447,25 @@ app.directive('cyTree', ['$log', '$compile', 'cyTreeUtils',
                         node.selected = true;
                     }
                 }
+
+                function setNodePartSelected(node) {
+                    node.partSelected = true;
+                    node.selected = false;
+                    node.nonSelected = false;
+                }
             }
         };
     }
 ]);
 
+/**
+ * @ngdoc directive
+ * @name cyTree.directive:treeData
+ * @restrict A
+ *
+ * @description
+ * Directive that generate tree recursively
+ */
 app.directive('treeData', ['$q', '$log', '$compile', 'cyTreeUtils',
 
     function($q, $log, $compile, cyTreeUtils) {
@@ -298,7 +479,7 @@ app.directive('treeData', ['$q', '$log', '$compile', 'cyTreeUtils',
 
                 var treeTemplate = isSubTree ? cyTreeUtils.templateCache(scope.treeInfo.treeId) : 
                     cyTreeUtils.genTemplate(attrs.treeData, attrs.treeChildFlag || 'list', attrs.treeLabelFlag || 'name');
-                isSubTree = true;
+
                 //Rendering template.
                 element.append($compile(treeTemplate)(scope));
 
